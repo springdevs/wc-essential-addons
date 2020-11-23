b89df5200af38ae6b49926eef9a1d180787a0cda
Istiaq Nirab <istiaqanirab202@gmail.com>
2020-11-23 08:40:51 +0600

    remove all modules

--- modules/simple-subscription/requirements.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-if (!wp_next_scheduled('subscrpt_daily_cron')) {
-    wp_schedule_event(time(), 'daily', 'subscrpt_daily_cron');
-}
--- modules/simple-subscription/uninstall.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-wp_clear_scheduled_hook('subscrpt_daily_cron');
