789d8078b9036548b8e1b2393a607842c5875e66
ok9xnirab <istiaqanirab202@gmail.com>
2021-05-03 14:51:28 +0600

    update database.php

--- includes/Database.php
+++ includes/Database.php
@@ -46,10 +46,27 @@ class Database
             $query .= " WHERE {$this->selector_column}={$this->selector_value}";
         }
 
+        if (isset($this->args['order'])) {
+            $query .= " ORDER BY {$this->args['order']}";
+        }
+
         if (isset($this->args['limit'])) {
             $query .= " LIMIT {$this->args['limit']}";
         }
 
         return $wpdb->get_results($query);
+    }
+
+    public function count()
+    {
+        global $wpdb;
+        $table_name = $wpdb->prefix . $this->table;
+        $query = "SELECT count(id) AS count from $table_name";
+
+        if ($this->selector_column) {
+            $query .= " WHERE {$this->selector_column}={$this->selector_value}";
+        }
+
+        return $wpdb->get_results($query)[0]->count;
     }
 }
